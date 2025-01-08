ea2a5168458c1f00d2c191e4df007d1116536b4b
fivevfive2 <2499843808@qq.com>
2025-01-08 21:31:11 +0800

    sqlcoonpool 的实现

--- Sqlconnpool.hpp
+++ Sqlconnpool.hpp
@@ -7,12 +7,35 @@
 #include <chrono>
 #include <memory>
 #include "mysql/jdbc.h"
-namespace bre{
-    class Sqlconnpool{
-        public:
-            std::unique_lock<sql::Connection> Getinstance(){
-                
-            }
+#include "Config.hpp"
+#include <atomic>
+namespace bre
+{
+    class MySqlPool
+    {
+    public:
+        MySqlPool(const std::string &Url, const std::string &User, const std::string &Pass,
+                  const std::string &Schema, int PoolSize = 8)
+        {
+        }
 
+        std::unique_ptr<sql::Connection> GetConn() {}
+        void FreeConn(std::unique_ptr<sql::Connection> con) {}
+        void Close() {}
+        ~MySqlPool() {}
+
+    private:
+        void checkConnection() {}
+
+    private:
+        std::string url;
+        std::string user;
+        std::string pass;
+        std::string schema;
+        int poolSize;
+        std::queue<std::unique_ptr<sql::Connection>> pool;
+        std::mutex mux;
+        std::condition_variable cond;
+        std::atomic<bool> b_stop;
     };
 };
